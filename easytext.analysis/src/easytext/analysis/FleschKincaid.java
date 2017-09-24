aeb32958a1b01e7919927f667a64086ab5da776d
Jswizzy <smithjustinn@hotmail.com>
2017-09-23 21:39:29 -0400

    Initial commit

---
+++ easytext.analysis/src/easytext/analysis/FleschKincaid.java
@@ -0,0 +1,35 @@
+package easytext.analysis;
+
+import java.util.List;
+
+public class FleschKincaid {
+
+    public double analyze(List<List<String>> sentences) {
+        float totalsentences = sentences.size();
+        float totalwords = sentences.stream().mapToInt(List::size).sum();
+        float totalsyllables = sentences.stream()
+                .flatMapToInt(sentence ->
+                        sentence.stream().mapToInt(this::countSyllables))
+                .sum();
+        return 206.835 - 1.015 * (totalwords / totalsentences) - 84.6 * (totalsyllables / totalwords);
+    }
+
+    private int countSyllables(String word) {
+        int syllables = 0;
+        boolean prevNonVowel = false;
+        for(int i = 0; i < word.length(); i++) {
+            boolean isVowel = isVowel(word.toLowerCase().charAt(i));
+            if(prevNonVowel && isVowel && i != word.length() - 1) {
+                syllables++;
+            }
+            prevNonVowel = !isVowel;
+        }
+        syllables = syllables == 0 ? 1 : syllables;
+        return syllables;
+    }
+
+    private boolean isVowel(char letter) {
+        return letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u';
+    }
+
+}
---
+++ easytext.gui/src/module-info.java
@@ -0,0 +1,7 @@
+module easytext.gui {
+    exports easytext.gui to javafx.graphics;
+
+    requires easytext.analysis;
+    requires javafx.graphics;
+    requires javafx.controls;
+}
